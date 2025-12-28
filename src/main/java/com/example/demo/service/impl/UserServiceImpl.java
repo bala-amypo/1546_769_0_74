341d08f54ab6395401bdb42b8fa31a12732ca792
bala-amypo <bala@amypo.in>
2025-12-28 10:43:31 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,4 +1,5 @@
 package com.example.demo.service.impl;
+
 import com.example.demo.entity.Role;
 import com.example.demo.entity.User;
 import com.example.demo.repository.RoleRepository;
@@ -6,37 +7,34 @@ import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import java.util.Map;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
-    private final PasswordEncoder encoder;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder encoder) {
+    public UserServiceImpl(UserRepository userRepository,
+                           RoleRepository roleRepository,
+                           PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
-        this.encoder = encoder;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
-    @Transactional
-    public User registerUser(Map<String, String> userData) {
-        String email = userData.get("email");
-        if(email==null || email.isEmpty()) throw new IllegalArgumentException("Email required");
-        if (userRepository.findByEmail(email).isPresent()) throw new IllegalArgumentException("Email already exists");
+    public User register(String email, String password) {
 
         User user = new User();
-        user.setName(userData.get("name"));
         user.setEmail(email);
-        user.setPassword(encoder.encode(userData.get("password")));
-        
-        Optional<Role> roleOpt = roleRepository.findByName("USER");
-        Role userRole = roleOpt.orElseGet(() -> roleRepository.save(new Role("USER")));
-        user.getRoles().add(userRole);
+        user.setPassword(passwordEncoder.encode(password));
+
+        Role role = roleRepository.findByName("USER")
+                .orElseGet(() -> roleRepository.save(new Role("USER")));
+
+        user.getRoles().add(role);
+
         return userRepository.save(user);
     }
 }
